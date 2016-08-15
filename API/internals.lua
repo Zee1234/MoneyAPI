250a5936354fc7dce8f16402e0dff57a72518f60
Zee1234 <zeeatgit@gmail.com>
2016-08-15 08:31:02 -0500

    Improve Failsafes, Improve Returns, Improve Formatting

--- API/internals.lua
+++ API/internals.lua
@@ -5,10 +5,11 @@ function providerCaller(a_user,a_world,a_function,...)
   if g_Config.UseForcedRelations then
     for _,v in ipairs(g_Users[a_user]:getForcedList()) do
       repeat
-        if called[v] then break end
         if not g_Providers[v] then LOGERROR("Configured Provider \""..v.."\" is not registered! Please fix MoneyAPI's Config or enable the missing provider!") break end
-        table.insert(returned,cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...))
-        called[v] = true
+        if called[v] then break end
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
       until true
     end
   end
@@ -16,10 +17,11 @@ function providerCaller(a_user,a_world,a_function,...)
   if g_Config.UseWorldRestrictions then
     for _,v in ipairs(g_Worlds[a_world]:getLinkedProviders()) do
       repeat
-        if called[v] then break end
         if not g_Providers[v] then LOGERROR("Configured Provider \""..v.."\" is not registered! Please fix MoneyAPI's Config or enable the missing provider!") break end
-        table.insert(returned,cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...))
-        called[v] = true
+        if called[v] then break end
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
       until true
     end
   end
@@ -28,11 +30,60 @@ function providerCaller(a_user,a_world,a_function,...)
     for _,v in pairs(g_Providers) do
       repeat
         if called[v] then break end
-        table.insert(returned,cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...))
-        called[v] = true
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
       until true
     end
   end
   
-  return returned
+  return returned, called
+end
+
+
+function bankProviderCaller(a_user,a_world,a_function,...)
+  local called = {}
+  local returned = {}
+  
+  if g_Config.UseForcedRelations then
+    for _,v in ipairs(g_Users[a_user]:getForcedList()) do
+      repeat
+        if not g_Providers[v] then LOGERROR("Configured Provider \""..v.."\" is not registered! Please fix MoneyAPI's Config or enable the missing provider!") break end
+        if called[v] then break end
+        if not g_Providers[v]:hasBank() then break end
+        
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
+      until true
+    end
+  end
+  
+  if g_Config.UseWorldRestrictions then
+    for _,v in ipairs(g_Worlds[a_world]:getLinkedProviders()) do
+      repeat
+        if not g_Providers[v] then LOGERROR("Configured Provider \""..v.."\" is not registered! Please fix MoneyAPI's Config or enable the missing provider!") break end
+        if called[v] then break end
+        if not g_Providers[v]:hasBank() then break end
+        
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
+      until true
+    end
+  end
+  
+  if not g_Config.UseForcedRelations and not g_Config.UseWorldRestrictions then
+    for _,v in pairs(g_Providers) do
+      repeat
+        if called[v] then break end
+        if not g_Providers[v]:hasBank() then break end
+        local ret = cPluginManager:CallPlugin(v,"MoneyAPIProviderCallback",a_function,...)
+        table.insert(returned,ret)
+        called[v] = ret
+      until true
+    end
+  end
+  
+  return returned, called
 end
