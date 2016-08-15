d43322551e3b538486ecc749558d8e626278dd11
Zee1234 <zeeatgit@gmail.com>
2016-08-15 14:56:00 -0500

    Removed LDoc because annoying

--- Registration.lua
+++ Registration.lua
@@ -1,4 +1,4 @@
---====================Registration API calls
+--====================User Registration API calls
 
 
 --- Register a user (store) plugin.
@@ -20,5 +20,35 @@ function UnregisterUser(a_plugin)
   if not g_Users[a_plugin] then return false, "Plugin name already unregistered" end
   
   g_Users[a_plugin] = nil
+  return true
+end
+
+
+
+--====================Provider Registration API calls
+
+
+
+--- Register a provider (currency) plugin.
+--@param a_plugin plugin name (string)
+--@param a_bankSupport do you have bank support (bool)
+--@return true if successful, false and an error string otherwise
+function RegisterProvider(a_plugin,a_bankSupport)
+  assert(type(a_plugin) == "string","RegisterUser recieved non-string value!")
+  if g_Providers[a_plugin] then return false, "Plugin name already registered" end
+  
+  g_Providers[a_plugin] = cProvider:new(a_plugin)
+  return true
+end
+
+
+--- Unregister a provider (currency) plugin.
+--@param a_plugin plugin name (string)
+--@return true if successful, false and an error string otherwise
+function UnregisterProvider(a_plugin)
+  assert(type(a_plugin) == "string","UnregisterProvider recieved non-string value!")
+  if not g_Providers[a_plugin] then return false, "Plugin name already unregistered" end
+  
+  g_Providers[a_plugin] = nil
   return true
 end
