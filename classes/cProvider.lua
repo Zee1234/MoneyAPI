d6b5be2c8af69b340e072c363001aa4a008238a5
Zee1234 <zeeatgit@gmail.com>
2016-08-15 08:33:01 -0500

    Add bank support

--- classes/cProvider.lua
+++ classes/cProvider.lua
@@ -1,12 +1,13 @@
 local cProvider = {}
 cProvider.__index = cProvider
 
-function cProvider:new(a_plugin)
+function cProvider:new(a_plugin,a_bankSupport)
   assert(type(a_name) == "string","cDependent:new() recieved a non-string value for a_name! Value recieved:" .. tostring(a_name))
   local obj = {}
   setmetatable(obj,cProvider)
   
   obj.name = a_plugin
+  obj.bank = a_bankSupport
   
   
   return obj
@@ -14,6 +15,10 @@ end
 
 function cProvider:getName()
   return self.Name
+end
+
+function cProvider:hasBank()
+  return self.bank
 end
 
 
