d2c57ff0ec3a0cbf1f7d32378ddc0a7bd44e2e6e
Zee1234 <zeeatgit@gmail.com>
2016-08-15 12:35:27 -0500

    Update Globals

--- globals.lua
+++ globals.lua
@@ -1,3 +1,13 @@
 g_Providers = {}
 g_Users = {}
 g_Worlds = {}
+g_Plugin = {}
+
+
+cProvider =  dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/classes/cProvider.lua")
+cUser =  dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/classes/cUser.lua")
+cMyWorld =  dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/classes/cMyWorld.lua")
+
+dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/API/internals.lua")
+dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/API/Provider.lua")
+dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/API/User.lua")
