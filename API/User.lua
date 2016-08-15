fb0b2f27d9ba9a07d5cdb33bbdd4c805b1db6f51
Zee1234 <zeeatgit@gmail.com>
2016-08-15 08:29:08 -0500

    Skeleton of most/all user API calls

---
+++ API/User.lua
@@ -0,0 +1,105 @@
+--====================Registration Control
+function RegisterUser()
+  
+end
+
+function UnregisterUser()
+  
+end
+
+
+
+
+
+
+--====================General API Calls
+
+function CurrencyName()
+  
+end
+
+function CurrencyPlural()
+  
+end
+
+function Format()
+  
+end
+
+function GetPlayers()
+
+end
+
+function GetBanks()
+  
+end
+
+function GetName()
+  
+end
+
+function HasBankSupport()
+  
+end
+
+function HasFractionalDigits()
+  
+end
+
+function IsEnabled()
+  
+end
+
+
+
+--====================Player API Calls
+
+function PlayerBalance()
+  
+end
+
+function PlayerDeposit()
+  
+end
+
+function PlayerWithdraw()
+  
+end
+
+
+
+--====================Bank API Calls
+
+function BankBalance(a_bank,a_player,a_world)
+  
+end
+
+function BankDeposit(a_bank,a_player,a_world,a_value)
+  
+end
+
+function BankHasMember(a_bank,a_player,a_world,a_value)
+  
+end
+
+function BankMembers(a_bank,a_world)
+  
+end
+
+function BankOwner(a_bank,a_world)
+  
+end
+
+function BanksWithMember(a_player,a_world)
+  
+end
+
+function BankWithdraw(a_bank,a_player,a_world,a_value)
+  
+end
+
+function BanksWithPlayer(a_player,a_world)
+  
+end
+
+
