cadae0eb63939afd6663515cb1b741118ab6ff4e
Zee1234 <zeeatgit@gmail.com>
2016-08-14 22:56:49 -0500

    Setup Config and Config Loader

--- config.lua
+++ config.lua
@@ -1,1 +1,7 @@
-g_Config = {}
+local ZPSI = dofile(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/libs/ZPSI/ZPSI.lua")
+
+g_Config = ZPSI.parse(cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/config.zpsi").Config
+
+assert(g_Config.UseWorldRestrictions ~= nil,"Config value UseWorldRestrictions not found")
+assert(g_Config.UseForcedRelations ~= nil,"Config value UseForcedRelations not found")
+assert(g_Config.StrictWorlds ~= nil,"Config value UseWorldRestrictions not found")
