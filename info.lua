cafe34ca2dfc753343f28a90726235eda66f7ae2
Zee1234 <zeeatgit@gmail.com>
2016-08-23 21:26:34 -0500

    Fix comment type and add info.lua

---
+++ info.lua
@@ -0,0 +1,17 @@
+g_PluginInfo =
+{
+	Name = "MoneyAPI",
+	Date = "2016-08-23",
+	Description = "A plugin that allows currency interactions to be centralized and standardized.",
+
+	-- The following members will be documented in greater detail later:
+	AdditionalInfo = {
+    {
+      Title = "Configuration",
+      Contents = "Read the config.zpsi file, it explains all. Even so, default configuration is highly recommended."
+    },
+  },
+	Commands = {},
+	ConsoleCommands = {},
+	Permissions = {},
+}
